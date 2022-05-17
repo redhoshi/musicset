032ca0f23ca71a3019319e83da0832ad8a4d128f
redhoshi <rosematrix0910o@gmail.com>
2022-05-17 11:02:28 +0900

    guard erro->firebase_storageのversionあげたら治った

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'config/config.dart';
 
 final configurations = Configurations();
@@ -16,12 +16,10 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-/*firebase_storage.FirebaseStorage storage =
-    firebase_storage.FirebaseStorage.instance;*/
+firebase_storage.FirebaseStorage storage =
+    firebase_storage.FirebaseStorage.instance;
 
 class MyApp extends StatelessWidget {
-  //const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
